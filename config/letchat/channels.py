656c248271b68133808e26a2b50151dbed295f64
swallahfx <adeniyisaheed75@gmail.com>
2022-01-31 01:46:45 +0100

    chatapp-simple

---
+++ config/letchat/channels.py
@@ -0,0 +1,31 @@
+import os
+
+import django
+from channels.http import AsgiHandler
+from channels.routing import ProtocolTypeRouter
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'letchat.settings')
+django.setup()
+
+application = ProtocolTypeRouter({
+  "http": AsgiHandler(),
+  # Just HTTP for now. (We can add other protocols later.)
+})
+
+import os
+
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from django.core.asgi import get_asgi_application
+import chat.routing
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "letchat.settings")
+
+application = ProtocolTypeRouter({
+  "http": get_asgi_application(),
+  "websocket": AuthMiddlewareStack(
+        URLRouter(
+            chat.routing.websocket_urlpatterns
+        )
+    ),
+})
